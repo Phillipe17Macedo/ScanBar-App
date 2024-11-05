00d58c952a383993d71f444c809d518751a1368f
Phillipe Macedo <63377161+Phillipe17Macedo@users.noreply.github.com>
2024-11-05 08:59:51 -0300

    melhorando-app

--- App.tsx
+++ App.tsx
@@ -29,15 +29,70 @@ export default function App() {
     getCameraPermissions();
   }, []);
 
+  const mapBarcodeType = (type: string | number): string => {
+    const typeMapping: { [key: string]: string } = {
+      // EAN-13
+      "org.gs1.EAN-13": "EAN-13",
+      "32": "EAN-13",
+
+      // EAN-8
+      "org.gs1.EAN-8": "EAN-8",
+      "13": "EAN-8",
+
+      // UPC-A
+      "org.gs1.UPC-A": "UPC-A",
+      "12": "UPC-A",
+
+      // UPC-E
+      "org.gs1.UPC-E": "UPC-E",
+      "9": "UPC-E",
+
+      // Code 39
+      "org.iso.Code39": "Code 39",
+      "39": "Code 39",
+
+      // Code 93
+      "org.iso.Code93": "Code 93",
+      "93": "Code 93",
+
+      // Code 128
+      "org.iso.Code128": "Code 128",
+      "128": "Code 128",
+
+      // ITF (Interleaved 2 of 5)
+      "org.iso.ITF": "ITF",
+      "25": "ITF",
+
+      // PDF417
+      "org.iso.PDF417": "PDF417",
+      "57": "PDF417",
+
+      // Aztec Code
+      "org.iso.Aztec": "Aztec",
+      aztec: "Aztec",
+
+      // Data Matrix
+      "org.iso.DataMatrix": "Data Matrix",
+      datamatrix: "Data Matrix",
+
+      // Codabar
+      "org.iso.Codabar": "Codabar",
+      codabar: "Codabar",
+    };
+
+    return typeMapping[type] || "Unknown";
+  };
+
   const handleBarcodeScanned = async ({ type, data }: any) => {
     setScanned(true);
     setLoading(true);
 
     const codigoBarra = {
-      tipo: String(type),
+      tipo: mapBarcodeType(type),
       codigo: data,
       dataLeitura: new Date().toISOString(),
     };
+    console.log("Informações: ", codigoBarra);
 
     try {
       const response = await cadastrarCodigoBarra(codigoBarra);
@@ -85,7 +140,6 @@ export default function App() {
           onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
           barcodeScannerSettings={{
             barcodeTypes: [
-              "pdf417",
               "ean13",
               "ean8",
               "upc_a",
@@ -94,6 +148,8 @@ export default function App() {
               "code39",
               "code93",
               "itf14",
+              "pdf417",
+              "codabar",
             ],
           }}
           enableTorch={flashActive}
